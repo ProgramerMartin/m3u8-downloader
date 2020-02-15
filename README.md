2dc79213a1248a1681d8b36a8389644d5dc8a75a
sndnvaps <sndnvaps@gmail.com>
2020-02-15 18:23:46 +0800

    更新代码，使生成的ts文件名更规范；修复windows平台生成mp4文件不连续问题；定义-ht参数，用于指定ts文件的url连接

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # m3u8-downloader
 
-golang 多线程下载直播流m3u8格式的视屏，跨平台（建议unix上运行）
+golang 多线程下载直播流m3u8格式的视屏，跨平台
 
 > 以下载岛国小电影哦  
 > 可以下载岛国小电影哦  
@@ -9,51 +9,37 @@ golang 多线程下载直播流m3u8格式的视屏，跨平台（建议unix上�
 
 ### 运行
 
-`go run m3u8.go -u "m3u8的url" -o "下载的电影名[默认：url截取的名字]" -n 80`
-
+```bash
+$go build -o m3u8-downloader
+$m3u8-downloader  -u="m3u8的url" -o="下载的电影名[默认：url截取的名字]" -n=80
+$m3u8-downloader -u="m3u8的url" -o="下载的电影名[默认：url截取的名字]" -n=80 -ht="apiv1"
+$m3u8-downloader -u="m3u8的url" -o="下载的电影名[默认：url截取的名字]" -n=80 -ht="apiv2"
+```
 
 ### 功能介绍
 
-1. 多线程下载m3u8的ts片段（加密的同步解密）
+1. 多线程下载m3u8的ts片段（加密的同步解密）-> 加密功能目前有些问题，正在尝试修复
 2. 合并下载的ts文件
 
 
 ### 可能遇到的异常、解决方法 (看@todo）
 
-1. 下载失败的情况【详情请见代码里面：get_host方法】
+1. 下载失败的情况,请设置 -ht="apiv1" 或者 -ht="apiv2"
 
 ```golang
-
-func get_host(Url string) string {
+func get_host(Url string, ht string) string {
 	u, err := url.Parse(Url)
+	var host string
 	check(err)
-	//@todo [see here 选择注释以下其中之一，主要用来构建不同的ts_url地址]
-	return u.Scheme + "://" + u.Host + path.Dir(u.Path)
-	return u.Scheme + "://" + u.Host
+	switch ht {
+	case "apiv1":
+		host = u.Scheme + "://" + u.Host + path.Dir(u.Path)
+	case "apiv2":
+		host = u.Scheme + "://" + u.Host
+	}
+	return host
 }
 ```
 
-2. windows端运行下载的视屏顺序错乱
+2. windows端运行下载的视屏顺序错乱 -> 此问题已经修复
 
-```golang
-//unix合并文件
-func unix_merge_file(path string) {
-	os.Chdir(path)
-	ExecShell("rm -rf ad*.ts")
-	cmd := `ls  *.ts |sort -t "\." -k 1 -n |awk '{print $0}' |xargs -n 1 -I {} bash -c "cat {} >> new.tmp"`
-	ExecShell(cmd)
-	ExecShell("rm -rf *.ts")
-	os.Rename("new.tmp", "new.mp4")
-}
-
-//windows合并文件
-//@todo [dos命令不熟，可能导致顺序乱，dos大神可仿照linux的合并方法unix_merge_file做调整，或者手动合并 O(∩_∩)O~~]
-func win_merge_file(path string) {
-	os.Chdir(path)
-	ExecShell("del /Q ad*.ts")
-	ExecShell("copy /b *.ts new.tmp")
-	//ExecShell("del /Q *.ts")
-	ExecShell("del /Q *.mp4")
-	os.Rename("new.tmp", "new.mp4")
-}
-```
--- m3u8.go
+++ m3u8.go
@@ -3,23 +3,23 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/levigross/grequests"
-	"log"
-	"os"
-	"regexp"
-	"strings"
-	"time"
-	"os/exec"
 	"bytes"
-	"net/url"
-	"path"
-	"runtime"
-	"sync"
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/levigross/grequests"
+	"gopkg.in/urfave/cli.v1"
 )
 
 const (
@@ -27,10 +27,6 @@ const (
 )
 
 var (
-	urlFlag = flag.String("u", "", "url of m3u8")
-	nFlag   = flag.Int("n", 80, "max goroutines num")
-	oFlag   = flag.String("o", "new", "name of the download  movie")
-
 	logger *log.Logger
 	ro     = &grequests.RequestOptions{
 		UserAgent:      "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -38,11 +34,22 @@ var (
 		Headers: map[string]string{
 			"Connection":      "keep-alive",
 			"Accept":          "*/*",
-			"Accept-Encoding": "br, gzip, deflate",
+			"Accept-Encoding": "*",
 			"Accept-Language": "zh-Hans;q=1",
 		},
 	}
 )
+
+//FileInfo 用于保存ts文件的下载地址和文件名
+type FileInfo struct {
+	FileName  string
+	TSFileURL string
+}
+
+//FileLists 用于保存所有TS文件的信息
+type FileLists struct {
+	FileInfos []FileInfo
+}
 
 func init() {
 	logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
@@ -99,7 +106,7 @@ func get_m3u8_body(Url string) string {
 }
 
 //获取m3u8加密的密钥
-func get_m3u8_key(html string) (key string) {
+func get_m3u8_key(html string, Url string, ht string) (key string) {
 	lines := strings.Split(html, "\n")
 	key = ""
 	for _, line := range lines {
@@ -108,7 +115,7 @@ func get_m3u8_key(html string) (key string) {
 			quotation_mark_pos := strings.LastIndex(line, "\"")
 			key_url := strings.Split(line[uri_pos:quotation_mark_pos], "\"")[1]
 			if !strings.Contains(line, "http") {
-				key_url = fmt.Sprintf("%s/%s", get_host(*urlFlag), key_url)
+				key_url = fmt.Sprintf("%s/%s", get_host(Url, ht), key_url)
 			}
 			logger.Println("key_url:", key_url)
 			res, err := grequests.Get(key_url, ro)
@@ -121,41 +128,45 @@ func get_m3u8_key(html string) (key string) {
 	return
 }
 
-func get_url_list(host, body string) (list []string) {
+func get_url_list(host, body string) (list FileLists) {
 	lines := strings.Split(body, "\n")
+	//临时变量，用于存放 line数据
+	var tmp FileInfo
+	index := 0
 	for _, line := range lines {
 		if !strings.HasPrefix(line, "#") && line != "" {
 			if strings.HasPrefix(line, "http") {
-				list = append(list, line)
+				tmp = FileInfo{
+					FileName:  fmt.Sprintf("%05d.ts", index),
+					TSFileURL: line,
+				}
+				list.FileInfos = append(list.FileInfos, tmp)
+				index++
 			} else {
-				list = append(list, fmt.Sprintf("%s/%s", host, line))
+				fmt.Println("get_url_list", index, line)
+				tmp = FileInfo{
+					FileName:  fmt.Sprintf("%05d.ts", index),
+					TSFileURL: fmt.Sprintf("%s/%s", host, line),
+				}
+				list.FileInfos = append(list.FileInfos, tmp)
+				index++
 			}
 		}
 	}
 	return
 }
 
-//去掉字符串所有的非数字的字符
-func TrimStr2int(str string) string {
-	newStr := strings.TrimRight(str, ".ts")
-	re3, _ := regexp.Compile("[a-zA-Z_]+")
-	intStr := re3.ReplaceAllString(newStr, "")
-	return intStr
-}
-
 //下载ts文件
-func download_ts_file(ts_url, download_dir, key string, retries uint) {
-	logger.Println("start ts_url:", ts_url, time.Now().Second())
+func download_ts_file(ts_url FileInfo, download_dir string, key string, retries uint) {
+	logger.Println("start ts_url:", ts_url.TSFileURL, time.Now().Second())
 
-	file_name := ts_url[strings.LastIndex(ts_url, "/"):]
-	file_name_new := TrimStr2int(file_name) + ".ts"
-	curr_path := fmt.Sprintf("%s%s", download_dir, file_name_new)
+	curr_path := fmt.Sprintf("%s/%s", download_dir, ts_url.FileName)
 	if isExist, _ := PathExists(curr_path); isExist {
 		logger.Println("[warn]: file already exist")
 		return
 	}
 
-	res, err := grequests.Get(ts_url, ro)
+	res, err := grequests.Get(ts_url.TSFileURL, ro)
 	if err != nil || !res.Ok {
 		if retries > 0 {
 			logger.Printf("[warn]Retry:%d, %s", retries-1, ts_url)
@@ -180,9 +191,21 @@ func download_ts_file(ts_url, download_dir, key string, retries uint) {
 	}
 }
 
-//执行shell
+//ExecShell 执行shell
 func ExecShell(s string) {
 	cmd := exec.Command("/bin/bash", "-c", s)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", out.String())
+}
+
+//ExecWinShell 执行shell
+func ExecWinShell(s string) {
+	cmd := exec.Command("cmd", "/C", s)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
@@ -206,10 +229,10 @@ func unix_merge_file(path string) {
 //@todo [dos命令不熟，可能导致顺序乱，dos大神可仿照linux的合并方法unix_merge_file做调整]
 func win_merge_file(path string) {
 	os.Chdir(path)
-	ExecShell("del /Q ad*.ts")
-	ExecShell("copy /b *.ts new.tmp")
-	//ExecShell("del /Q *.ts")
-	ExecShell("del /Q *.mp4")
+	ExecWinShell("del /Q ad*.ts")
+	ExecWinShell("copy /b *.ts new.tmp")
+	ExecWinShell("del /Q *.ts")
+	ExecWinShell("del /Q *.mp4")
 	os.Rename("new.tmp", "new.mp4")
 }
 
@@ -225,58 +248,63 @@ func PathExists(path string) (bool, error) {
 	return false, err
 }
 
-func get_host(Url string) string {
+func get_host(Url string, ht string) string {
 	u, err := url.Parse(Url)
+	var host string
 	check(err)
-	//@todo [see here 选择注释以下其中之一，主要用来构建不同的ts_url地址]
-	return u.Scheme + "://" + u.Host + path.Dir(u.Path)
-	return u.Scheme + "://" + u.Host
+	switch ht {
+	case "apiv1":
+		host = u.Scheme + "://" + u.Host + path.Dir(u.Path)
+	case "apiv2":
+		host = u.Scheme + "://" + u.Host
+	}
+	return host
 }
 
-func Run() {
-	logger.Println("============================ 功能：多线程下载直播流m3u8的视屏（ts+合并） ========================")
-	logger.Println("============================ 下载失败的情况【详情请见代码里面：get_host方法】 ========================")
+func Run(c *cli.Context) error {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	now := time.Now()
 
-	flag.Parse()
+	m3u8URL := c.String("url")
+	hosttype := c.String("hosttype")
+	outputPath := c.String("output")
+	Number := c.Int("number")
 
-	if !strings.HasPrefix(*urlFlag, "http") || !strings.Contains(*urlFlag, "m3u8") {
-		flag.Usage()
-		return
+	//先判断有没有设置 m3u8的下载地址
+	if !strings.HasPrefix(m3u8URL, "http") || !strings.Contains(m3u8URL, "m3u8") || m3u8URL == "" {
+		cli.ShowAppHelpAndExit(c, 0)
 	}
-	Url := *urlFlag
-	maxGoroutines := *nFlag
+
+	maxGoroutines := Number
 
 	//Url = "https://cn1.bb997.me/sehua/mywife-1458.m3u8"
 
 	pwd, _ := os.Getwd()
 	//pwd = "/Users/chao/Desktop"
 
-	movieDir := *oFlag
-	if *oFlag == "new" {
-		movieDir = path.Base(Url)
-	}
+	movieDir := outputPath
+
 	download_dir := pwd + "/movie/" + string(movieDir) + time.Now().Format("0601021504")
 	if isExist, _ := PathExists(download_dir); !isExist {
 		os.MkdirAll(download_dir, os.ModePerm)
 	}
 
-	host := get_host(Url)
-	body := get_m3u8_body(Url)
+	host := get_host(m3u8URL, hosttype)
 
-	key := get_m3u8_key(body)
+	body := get_m3u8_body(m3u8URL)
+
+	key := get_m3u8_key(body, m3u8URL, hosttype)
 	logger.Printf("key: %s", key)
 
 	url_list := get_url_list(host, body)
-	logger.Println("url_list:", url_list)
+	logger.Println("url_list:", url_list.FileInfos)
 
 	var wg sync.WaitGroup
 	limiter := make(chan struct{}, maxGoroutines)
-	for _, ts_url := range url_list {
+	for _, ts_url := range url_list.FileInfos {
 		wg.Add(1)
 		limiter <- struct{}{}
-		go func(ts_url, download_dir, key string, retryies uint) {
+		go func(ts_url FileInfo, download_dir string, key string, retryies uint) {
 			defer func() {
 				wg.Done()
 				<-limiter
@@ -300,8 +328,42 @@ func Run() {
 	}
 
 	logger.Printf("任务完成，耗时:%#vs\n", time.Now().Sub(now).Seconds())
+	return nil
 }
 
 func main() {
-	Run()
+
+	app := cli.NewApp()
+	app.Name = "golang m3u8 video Downloader"
+	app.Version = "1.0.0"
+
+	app.Copyright = "©2020 - present Jimes Yang<sndnvaps@gmail.com>"
+	app.Usage = "功能：多线程下载直播流m3u8的视屏（ts+合并）\n\t\t如果下载失败，请使用--hosttype定义get_host的类型"
+	app.Action = Run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "url,u",
+			Usage: "m3u8下载地址(http(s)://url/xx/xx/index.m3u8)",
+		},
+		cli.StringFlag{
+			Name:  "output,o",
+			Value: "outputs",
+			Usage: "定义存放的目录前序(目录名为{{.output}}0601021504)",
+		},
+		cli.StringFlag{
+			Name:  "hosttype,ht",
+			Value: "apiv1",
+			Usage: "设置get_host的方式(apiv1: `http(s):// + url.Host + path.Dir(url.Path)`; apiv2: `http(s)://+ u.Host`",
+		},
+		cli.IntFlag{
+			Name:  "number,n",
+			Value: 80,
+			Usage: "设置并发数量",
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
